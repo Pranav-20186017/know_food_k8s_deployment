5bcc1e8ae9077b05e49901cc85b7fab719f9dc3e
Pranav Surampudi <41884998+Pranav-20186017@users.noreply.github.com>
2020-01-07 14:25:30 +0530

    Update k8s-deploy.sh

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -7,8 +7,5 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 sudo minikube start --vm-driver none
-git clone https://github.com/Pranav-20186017/know_food_k8s_deployment.git
-cd know_food_k8s_deployment
 sudo minikube start --vm-driver none
 sudo kubectl apply -f deploy-py.yaml
-
